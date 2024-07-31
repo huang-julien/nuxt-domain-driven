4379c9e2d3ef66a0dd329b0e7b76254d44b9b284
Julien Huang <julien.h.dev@gmail.com>
2024-07-31 23:00:08 +0200

    feat: autoimports pages utils and components

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,87 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { readdirSync, lstatSync, existsSync } from 'node:fs'
+import { defineNuxtModule, addComponentsDir, addImportsDir } from '@nuxt/kit'
+import { join } from 'pathe'
+import { toVueRouter4 } from 'unrouting'
+import type { NuxtPage } from '@nuxt/schema'
 
-// Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  /**
+   * The directory to scan for domain-driven design files
+   * @default "src"
+   */
+  directory?: string
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'nuxt-domain-driven',
+    configKey: 'ddd',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+  async setup(options, nuxt) {
+    const { directory = 'src' } = options
+    const { rootDir } = nuxt.options
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    const directoryDir = join(rootDir, directory)
+    const contents = await readdirSync(directoryDir)
+    const registeredPages: NuxtPage[] = []
+
+    for (const content of contents) {
+      if (directoryExist(join(directoryDir, content))) {
+        if (directoryExist(join(directoryDir, content, 'components'))) {
+          addComponentsDir({
+            path: join(directoryDir, content, 'components'),
+            prefix: content,
+            watch: true,
+          })
+        }
+
+        const composableDir = join(directoryDir, content, 'composables')
+        if (directoryExist(composableDir)) {
+          addImportsDir(composableDir)
+        }
+
+        const utilsDir = join(directoryDir, content, 'utils')
+        if (directoryExist(utilsDir)) {
+          addImportsDir(utilsDir)
+        }
+
+        const pagesDir = join(directoryDir, content, 'pages')
+
+        if (directoryExist(pagesDir)) {
+          registeredPages.push(...registerPages(pagesDir, directoryDir, content))
+        }
+      }
+    }
+
+    nuxt.hook('pages:extend', (pages) => {
+      console.log(registeredPages, '!!!!!!!!!!!!!!')
+      pages.push(...registeredPages)
+    })
   },
 })
+
+function directoryExist(directory: string): boolean {
+  return existsSync(directory) && lstatSync(directory).isDirectory()
+}
+
+function registerPages(pagesDir: string, srcDir: string, prefix: string, pages: NuxtPage[] = []) {
+  const contents = readdirSync(pagesDir)
+
+  for (const content of contents) {
+    if (directoryExist(join(pagesDir, content))) {
+      registerPages(join(pagesDir, content), srcDir, prefix, pages)
+    }
+    else {
+      const { path } = toVueRouter4(join(pagesDir.split(srcDir)[1]))
+
+      pages.push({
+        path,
+        file: join(pagesDir, content),
+      })
+    }
+  }
+
+  return pages
+}
